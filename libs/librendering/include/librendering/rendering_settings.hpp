16e10a53f8607fc7c9ba8a32882662d044460838
Mykola Morozov <mcmikecreations@gmail.com>
2022-07-15 20:12:49 +0200

    Added code comments and explanations.

--- libs/librendering/include/librendering/rendering_settings.hpp
+++ libs/librendering/include/librendering/rendering_settings.hpp
@@ -4,15 +4,15 @@
 namespace librendering {
 	class rendering_settings {
 	public:
-		int frame_width = 640;
-		int frame_height = 480;
+		int frame_width = 640; /**< Rendered frame width */
+		int frame_height = 480; /**< Rendered frame height */
 
-		int window_width = 640;
-		int window_height = 480;
+		int window_width = 640; /**< Desktop window width */
+		int window_height = 480; /**< Desktop window height */
 
-		bool free_cam_enabled = true;
+		bool free_cam_enabled = true; /**< Is free flying camera enabled */
 
-		float popup_timeout = 5.f;
+		float popup_timeout = 5.f; /**< How long should popups stay on screen, seconds */
 	};
 }
 
